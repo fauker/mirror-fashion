f01966e43575babc11c14b5ed2be0c9f9bbf25e3
Lucas Moreira <eskerlucas1@gmail.com>
2015-11-30 12:48:55 -0200

    comments

--- gulpfile.js
+++ gulpfile.js
@@ -7,26 +7,32 @@ var gulp = require('gulp'),
   cssmin = require('gulp-cssmin'),
   browserSync = require('browser-sync');
 
+// Função padrão
 gulp.task('default', ['copy'], function() {
 	gulp.start('build-img', 'usemin');
 });
 
+// Copia todos os arquivos da src para a dist
 gulp.task('copy', ['clean'], function() {
 	return gulp.src('src/**/*')
 		.pipe(gulp.dest('dist'));
 });
 
+// Apaga a pasta dist
 gulp.task('clean', function() {
 	return gulp.src('dist')
 		.pipe(clean());
 });
 
+// Otimiza as imagens do diretório informado
 gulp.task('build-img', function() {
 	return gulp.src('dist/img/**/*')
 		.pipe(imagemin())
 		.pipe(gulp.dest('dist/img'));
 });
 
+// Minifica CSS e JS e troca o trecho do HTML que faz
+// a importação das bibliotecas
 gulp.task('usemin', function() {
 	 return gulp.src('dist/**/*.html')
 		.pipe(usemin({
@@ -36,6 +42,7 @@ gulp.task('usemin', function() {
 		.pipe(gulp.dest('dist'));
 });
 
+// Servidor sincronizado com o browser
 gulp.task('server', function() {
 	browserSync.init({
 		server: {
